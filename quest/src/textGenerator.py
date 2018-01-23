b1fe83ae63453fbf905e1b72b1a80b4372243e21
au16221 <ajay.unagar@zs.com>
2018-01-23 16:34:35 +0530

    changes in textGenerator.py

--- quest/src/textGenerator.py
+++ quest/src/textGenerator.py
@@ -1,8 +1,9 @@
 def file2Text(nameOfTheFile):
     import pandas as pd
     a = pd.read_csv(nameOfTheFile)
+    text = str(a['Text'][0]).decode('utf-8')
     # text = textract.process(nameOfTheFile).decode('utf-8')
-    # text = text.replace('\n',' ')
-    # text = text.replace('\t',' ')
-    # text = text.encode('ascii','ignore')
-    return str(a['Text'][0])
+    text = text.replace('\n',' ')
+    text = text.replace('\t',' ')
+    text = text.encode('ascii','ignore')
+    return text
